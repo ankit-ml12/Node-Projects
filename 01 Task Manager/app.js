ece71d1b1713cc2f49bd3473ba7707cd0afcfae2
ankit patidar <patiankit6555@gmail.com>
2023-03-06 17:07:43 +0530

    task manager mid way2

--- 01 Task Manager/app.js
+++ 01 Task Manager/app.js
@@ -2,13 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const MONGO_URI = process.env.MONGO_URI
 //middleware
 app.use(express.json())
 
 //routes
 app.use('/api/v1/tasks', tasks)
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`)
-})
+const start = async () => {
+  try {
+    await connectDB(MONGO_URI)
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
