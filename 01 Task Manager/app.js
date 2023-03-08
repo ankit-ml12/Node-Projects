2ab92520ae066677d860a6cc6959473afa6f5f5f
ankit patidar <patiankit6555@gmail.com>
2023-03-08 12:00:10 +0530

    task manager api complete

--- 01 Task Manager/app.js
+++ 01 Task Manager/app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const MONGO_URI = process.env.MONGO_URI
+
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/errorHandler')
 //middleware
 app.use(express.json())
 
 //routes
 app.use('/api/v1/tasks', tasks)
-
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 const start = async () => {
   try {
     await connectDB(MONGO_URI)
--- 01 Task Manager/controllers/tasks.js
+++ 01 Task Manager/controllers/tasks.js
@@ -1,62 +1,46 @@
+const asyncWrapper = require('../middleware/async')
 const Task = require('../models/Task')
-const getAllTasks = async (req, res) => {
-  try {
-    const task = await Task.find({})
-    res.status(200).json({ size: task.length, task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+const { createCustomError } = require('../errors/custom-error')
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const task = await Task.find({})
+  res.status(200).json({ size: task.length, task })
+})
+
+const creatTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+})
+
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id ${taskID}`, 404))
   }
-}
-const creatTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body)
-    res.status(201).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+  return res.status(200).json({ task })
+})
+
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params
+  //we pass third parameter to update the return task value and run the validator from the schema
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+  if (!task) {
+    return next(createCustomError(`No task with id ${taskID}`, 404))
   }
-}
-const getTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params
-    const task = await Task.findOne({ _id: taskID })
-    console.log(task)
-    if (!task) {
-      return res.status(404).json({ msg: `No task found with id ${taskID}` })
-    }
-    return res.status(200).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+  return res.status(200).json({ task })
+})
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params
+  const task = await Task.findByIdAndDelete({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id ${taskID}`, 404))
   }
-}
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params
-    //we pass third parameter to update the return task value and run the validator from the schema
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-    })
-    console.log(task)
-    if (!task) {
-      return res.status(404).json({ msg: `No task found with id ${taskID}` })
-    }
-    return res.status(200).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
-}
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params
-    const task = await Task.findByIdAndDelete({ _id: taskID })
-    console.log(task)
-    if (!task) {
-      return res.status(404).json({ msg: `No task found with id ${taskID}` })
-    }
-    return res.status(200).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
-}
+  return res.status(200).json({ task })
+})
 
 module.exports = { getAllTasks, creatTask, getTask, updateTask, deleteTask }
