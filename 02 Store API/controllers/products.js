e75fc6563a9ea671d5c6e43483773028588abec9
ankit patidar <patiankit6555@gmail.com>
2023-03-09 08:44:17 +0530

    store api complete

--- 02 Store API/controllers/products.js
+++ 02 Store API/controllers/products.js
@@ -7,8 +7,9 @@ const getAllProductsStatic = async (req, res) => {
 
   res.status(200).send({ nbHits: products.length, products })
 }
+
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query
+  const { featured, company, name, sort, field, numericFilters } = req.query
   const queryObject = {}
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
@@ -26,6 +27,37 @@ const getAllProducts = async (req, res) => {
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else result = result.sort('createAt')
+
+  if (field) {
+    const fieldLists = field.split(',').join(' ')
+    result = result.select(fieldLists)
+  }
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  result.skip(skip).limit(limit)
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    )
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
   const products = await result
   res.status(200).send({ nbHits: products.length, products })
 }
