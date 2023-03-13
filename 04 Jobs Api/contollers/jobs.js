bf4f200f3a90332fc7d7e60a08f95f40f14e05fd
ankit patidar <patiankit6555@gmail.com>
2023-03-14 05:13:52 +0530

    job controller

--- 04 Jobs Api/contollers/jobs.js
+++ 04 Jobs Api/contollers/jobs.js
@@ -1,13 +1,22 @@
 const Job = require('../models/Job')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes, getStatusCode } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 const { login } = require('./auth')
+
 const getAllJobs = async (req, res) => {
   const job = await Job.find({ createdBy: req.User.userId }).sort('createdAt')
   res.status(StatusCodes.OK).json({ count: job.length, job })
 }
 const getJob = async (req, res) => {
-  res.send('get  jobs')
+  const {
+    User: { userId },
+    params: { id: jobId },
+  } = req
+  const job = await Job.findOne({ _id: jobId, createdBy: userId })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 const createJobs = async (req, res) => {
   req.body.createdBy = req.User.userId
@@ -15,10 +24,38 @@ const createJobs = async (req, res) => {
   await res.status(StatusCodes.OK).json({ job })
 }
 const updateJobs = async (req, res) => {
-  res.send('update jobs')
+  const {
+    body: { company, position },
+    User: { userId },
+    params: { id: jobId },
+  } = req
+  if (!company || !position) {
+    throw new BadRequestError('company and position name cannot be empty')
+  }
+  const job = await Job.findByIdAndUpdate(
+    {
+      _id: jobId,
+      createdBy: userId,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  )
+  if (!job) {
+    throw new BadRequestError(`No job found with ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
+
 const deleteJobs = async (req, res) => {
-  res.send('delete  jobs')
+  const {
+    User: { userId },
+    params: { id: jobId },
+  } = req
+  const job = await Job.findByIdAndRemove({ _id: jobId, createdBy: userId })
+  if (!job) {
+    throw new BadRequestError(`No job found with ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 
 module.exports = { getAllJobs, getJob, createJobs, updateJobs, deleteJobs }
--- 04 Jobs Api/errors/bad-request.js
+++ 04 Jobs Api/errors/bad-request.js
@@ -3,6 +3,7 @@ const { StatusCodes } = require('http-status-codes')
 class BadRequestError extends customAPIError {
   constructor(message) {
     super(message)
+    console.log(StatusCodes.BAD_REQUEST)
     this.statusCode = StatusCodes.BAD_REQUEST
   }
 }
