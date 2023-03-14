1df1652e3ee9a2031bb30524d2b16ca5cd7b7d46
ankit patidar <patiankit6555@gmail.com>
2023-03-14 07:53:04 +0530

    error middleware

--- 04 Jobs Api/middleware/error-handler.js
+++ 04 Jobs Api/middleware/error-handler.js
@@ -2,13 +2,22 @@ const { customAPIError } = require('../errors')
 
 const { StatusCode, StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
+  console.log('err', err)
+  let customError = {
+    //set Default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again leter',
+  }
   if (err instanceof customAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .send('Something went wrong try again later')
+  console.log('err', err)
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${err.keyValue} field, Please choose another one`
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json('Something went wrong try again later')
+  console.log(err.msg)
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
